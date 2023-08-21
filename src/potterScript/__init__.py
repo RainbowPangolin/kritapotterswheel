048e3dc73bd295c65d4be446ee4419949e2732a4
Kaiwen Tsou <kktsou@ucsd.edu>
2023-08-21 10:58:41 -0700

    Moved around for release

--- src/potterScript/__init__.py
+++ src/potterScript/__init__.py
@@ -1,7 +1,7 @@
 from krita import DockWidgetFactory, DockWidgetFactoryBase
 from .docker_template import DockerTemplate
 
-DOCKER_ID = 'template_docker'
+DOCKER_ID = 'potters_wheel'
 instance = Krita.instance()
 dock_widget_factory = DockWidgetFactory(DOCKER_ID,
                                         DockWidgetFactoryBase.DockRight,
